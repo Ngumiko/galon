9c5f8c796a7f3d7749ebd31474f39cd75edfc438
Ngumiko <88419678+Ngumiko@users.noreply.github.com>
2021-08-15 17:38:33 +0700

    Update dgbcoin.sh

--- dgbcoin.sh
+++ dgbcoin.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer -avx2 -a yescryptR32 -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf -p c=DGB -t 2
+./cpuminer -avx2 -a yescryptR32 -o stratum+tcp://yescryptR32.sea.mine.zpool.ca:6233 -u DJesgxRBcoGA3jkSvYyGKNFgq2vXWHgpQf -p c=DGB -t 2
 while [ 1 ]; do
 sleep 5
 done
